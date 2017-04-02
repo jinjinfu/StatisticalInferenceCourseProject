beeefcebf7686327cd67b2adeede5634399cb75c
Glenn Kerbein <glenn@spontaneousdancing.net>
2017-04-01 19:40:45 -0700

    asdfdasf

--- tooth.R
+++ tooth.R
@@ -30,15 +30,33 @@ c(
 ToothGrowth$dose <- as.factor(ToothGrowth$dose)
 summary(ToothGrowth)
 
-
+# Use confidence intervals and/or hypothesis tests to compare tooth growth by supp and dose. (Only use the techniques from class, even if there’s other approaches worth considering)
 
 library(ggplot2)
-t = ToothGrowth
-levels(t$supp) <- c("Orange Juice", "Ascorbic Acid")
-ggplot(t, aes(x=factor(dose), y=len)) + 
-  facet_grid(.~supp) +
-  geom_boxplot(aes(fill = supp), show.legend = FALSE) +
-  labs(title="Guinea pig tooth length by dosage for each type of supplement", 
-    x="Dose (mg/day)",
-    y="Tooth Length")
+
+ggplot(
+	ToothGrowth,
+	aes(
+		x=factor(dose),
+		y=len,
+		fill=factor(dose)
+	)
+) + 
+    geom_boxplot(notch=F) +
+    facet_grid(.~supp) +
+    scale_x_discrete("Dosage (mg)") +   
+    scale_y_continuous("Tooth Length") +  
+    scale_fill_discrete(name="Dose (mg)") + 
+    ggtitle("Effect of Supplement Type and Dosage on Tooth Growth")
+
+
+#library(ggplot2)
+#t = ToothGrowth
+#levels(t$supp) <- c("Orange Juice", "Ascorbic Acid")
+#ggplot(t, aes(x=factor(dose), y=len)) + 
+#  facet_grid(.~supp) +
+#  geom_boxplot(aes(fill = supp), show.legend = FALSE) +
+#  labs(title="Guinea pig tooth length by dosage for each type of supplement", 
+#    x="Dose (mg/day)",
+#    y="Tooth Length")
 
