c910b90dcf88809cc5615d069439e702e65884b8
Glenn Kerbein <glenn@spontaneousdancing.net>
2017-04-01 18:51:14 -0700

    Clean up

--- tooth.R
+++ tooth.R
@@ -1,4 +1,19 @@
 #tooth.R
 
 ## Load ToothGrowth data and perform exploratory data analyses
+library(datasets)
+data(ToothGrowth)
+str(ToothGrowth)
+head(ToothGrowth)
+summary(ToothGrowth)
+
+library(ggplot2)
+t = ToothGrowth
+levels(t$supp) <- c("Orange Juice", "Ascorbic Acid")
+ggplot(t, aes(x=factor(dose), y=len)) + 
+  facet_grid(.~supp) +
+  geom_boxplot(aes(fill = supp), show.legend = FALSE) +
+  labs(title="Guinea pig tooth length by dosage for each type of supplement", 
+    x="Dose (mg/day)",
+    y="Tooth Length")
 
